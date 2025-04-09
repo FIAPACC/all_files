782fa842053cfcbdd8e40e636e146ab3f84da965
FIAPACC <fiapofficialacc@gmail.com>
2025-04-09 14:27:05 -0300

    Update cpAlexandre1.py

--- cpAlexandre1.py
+++ cpAlexandre1.py
@@ -1,27 +1,31 @@
+#QUESTÃO 1
 value = float(input('Escreva um preço em reais: '))
 
 print(f'Valor em Dolar: {(value * 0.17):.2f} \nValor em Euro: {(value * 0.15):.2f}')
 print(f'Valor em Peso Argetino: {(value * 181.78):.2f} \nValor em Libra Esterlina: {(value * 0.13):.2f}')
 print(f'Valor em Iene: {(value * 25):.2f}')
 
-num = int(input('Digite um número inteiro: '))
-u = 0
-d = 0
-c = 0
+#QUESTÃO 2
+num = int(input('\nDigite um número inteiro(0-999): '))
 
-while num >= 100:
-    c += 100
-    num -= 100
-while num >= 10:
-    d += 10
-    num -= 10
-while num >= 1:
-    u += 1
-    num -= 1
+c = (num // 100) * 100 if num >= 100 else 0
+d = ((num % 100) // 10) * 10 if num >= 10 else 0
+u = num % 10
 
+print(f"Centena: {c}")
+print(f"Dezena: {d}")
+print(f"Unidade: {u}")
 
+#QUESTÃO 3
+y = int(input('\nVamos calcular, em dias, a sua idade.\nComesse dizendo quantos anos você tem:' ))
+m = int(input('Agora, quantos meses inteiros se passaram depois do dia do seu último aniversário: '))
+d = int(input('E por fim, quantos dias que se passaram desde a última vez que passou o dia(e somente o dia) do seu aniversário: '))
 
-print(c)
-print(d)
-print(u)
-print('a')
+print('Sua idade em dias é de ' + str( y * 365 + m * 30 + d) + '.')
+
+#QUESTÃO 4
+d = int(input('Agora diga um número(dias) para ser convertido em anos, meses e dias: '))
+y = d//365
+m = (d - y * 365)//30
+d = d - y * 365 - m * 30
+print(f'Este número equivale a {y} ano(s), {m} mese(s) e {d} dia(s).')
